ae6fa813e136595a74965581f0dbb639eabbd0d7
Rashed Al-Masri <157921405+BloodyFlakes@users.noreply.github.com>
2024-08-22 21:57:32 +0300

    Add the login and signup logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login/Login";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Chat from "./pages/Chat/Chat";
+import Login from "./pages/Login/Login";
 import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        navigate("/chat");
+      } else {
+        navigate("/");
+      }
+    });
+  }, [navigate]);
+
   return (
     <>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import assets from "../../assets/assets";
 import "./Login.css";
+import { login, signUp } from "../../config/firebase";
 
 type AssetsType = {
   logo_big: string;
@@ -8,18 +9,51 @@ type AssetsType = {
 
 function Login() {
   const [currState, setCurrState] = useState<string>("Sign up");
+  const [userName, setUserName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (currState === "Sign up") {
+      signUp(userName, email, password);
+    } else {
+      login(email, password);
+    }
+  };
 
   return (
     <div className="login">
       <img src={(assets as AssetsType).logo_big} alt="logo" />
-      <form className="login-form">
+      <form onSubmit={onSubmitHandler} className="login-form">
         <h2>{currState}</h2>
 
         {currState === "Sign up" ? (
-          <input type="text" placeholder="Username" className="form-input" required />
+          <input
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+            type="text"
+            placeholder="Username"
+            className="form-input"
+            required
+          />
         ) : null}
-        <input type="email" placeholder="Email address" className="form-input" required />
-        <input type="password" placeholder="Password" className="form-input" required />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          placeholder="Email address"
+          className="form-input"
+          required
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          placeholder="Password"
+          className="form-input"
+          required
+        />
         <button type="submit">{currState === "Sign up" ? "Create account" : "Login now"}</button>
 
         <div className="login-term">
